d8d32debdfe2139243bbaaa9989cebdba7cb5d72
Megan (AKA Pilahi) Moran <kamokumoran@gmail.com>
2020-08-01 21:03:25 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,13 +6,14 @@ end
 def summon_captain_planet(array)
   planeteer_calls = []
   array.collect do |calls|
-    planeteer_calls << "#{calls.capitalize}!" #could maybe use push
+    planeteer_calls << "#{calls.capitalize}!" #could maybe use push but like last time, like this better if it works
 end
   planeteer_calls #call on it
 end
 
 def long_planeteer_calls(array)
-  # Your code here
+  array.any? do |word| #if any is TRUE
+    word.length > 4 #wind and fire not gonna come up
 end
 
 def find_the_cheese(array)
