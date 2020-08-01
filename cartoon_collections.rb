459dfa29fe389ae214bcdd8d3c79564b3bc7f1ae
Megan (AKA Pilahi) Moran <kamokumoran@gmail.com>
2020-08-01 21:29:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,6 +18,7 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find do |cheese| 
+    cheese == "cheddar" || cheese ==  "gouda"|| cheese == "camembert"
+  #cheese_types = ["cheddar", "gouda", "camembert"]
 end
